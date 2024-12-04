13557a58fd18f7dabda75365c0fafb756e2facc8
imjustjayk <brycekrause@icloud.com>
2024-12-04 17:51:33 -0600

    development

--- website/index.js
+++ website/index.js
@@ -1,28 +1,8 @@
-# TODO: 
-
-import random
-
-numbers = str("1234567890")
-symbols = str("!@#$%^&*()-~+><")
-letters = str("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-all_characters = []
-for char in numbers:    
-    all_characters.append(char)
-for char in symbols:
-    all_characters.append(char)
-for char in letters: 
-    all_characters.append(char)
-for char in letters.lower():
-    all_characters.append(char)
-
-
-
-def generate_password(length):
-    password = []
-    for x in range(int(length)):
-        password.append(random.choice(all_characters))
-    result = ''.join(password)
-    print(result)
-        
-
-    return result
+container = document.getElementById('container');
+output_area = document.getElementById('output_container');
+generate_button = document.getElementById("generate");
+generate_button.addEventListener('click', function(){
+    output = document.createElement('p');
+    output.textContent = 'pasword';
+    output_area.appendChild(output);
+});
