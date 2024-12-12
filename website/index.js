795386521f2e874c8d0739c611be6576e05b3fb2
imjustjayk <brycekrause@icloud.com>
2024-12-12 17:23:26 -0600

    ensure one checkbox is checked at all times

--- website/index.js
+++ website/index.js
@@ -74,3 +74,16 @@ document.addEventListener("DOMContentLoaded", function(){
     generate_password(length_slider.value);
 });
 
+function checkboxListener(event) { 
+    const checkboxes = ["special_check", "lowercase_check", "uppercase_check", "numbers_check"]; 
+    const checkedCheckboxes = checkboxes.filter(id => document.getElementById(id).checked); 
+    if (checkedCheckboxes.length === 0 && event.target.checked === false) { 
+        event.target.checked = true; 
+    } 
+    generate_password(length_slider.value); 
+} 
+const checkboxes = ["special_check", "lowercase_check", "uppercase_check", "numbers_check"]; 
+checkboxes.forEach(id => { 
+    const checkbox = document.getElementById(id); 
+    checkbox.addEventListener("change", checkboxListener); 
+});
